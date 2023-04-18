09159b33cc62edce72d4f72f7d015435c1d4fef0
Jazmin Eubanks <jazmin.eubanks@gmail.com>
2023-04-18 20:15:30 +0000

    commiting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 
   get("/users", { :controller => "users", :action => "index"})
+
   get("/users/:path_username", { :controller => "users", :action => "show" })
+
   get("/photos", { :controller => "photos", :action => "index" })
-  
+
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
+
+  get("/delete_photo/:path_id", { :controller => "photos", :action => "baii"})
+
+
 end
