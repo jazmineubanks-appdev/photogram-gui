2135a1f08cc9fb94d506d80c7beb9ecfabb1d802
Jazmin Eubanks <jazmin.eubanks@gmail.com>
2023-04-19 16:06:15 +0000

    71percent

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-
+  get("/", { :controller => "users", :action => "index"})
   get("/users", { :controller => "users", :action => "index"})
 
   get("/users/:path_username", { :controller => "users", :action => "show" })
+  get("/insert_user_record", { :controller => "users", :action => "create"})
+
+get("/update_user/:modify_id", { :controller => "users", :action => "update"})
 
   get("/photos", { :controller => "photos", :action => "index" })
 
@@ -14,5 +17,5 @@ Rails.application.routes.draw do
 
   get("/update_photo/:modify_id", { :controller => "photos", :action => "update"})
 
-
+  get("/insert_comment_record", { :controller => "photos", :action => "new_comment" })
 end
